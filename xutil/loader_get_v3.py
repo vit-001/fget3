6fadad3fa967ba1fdffdc88def5938d2343e01ac
vit-001 <vit-001@users.noreply.github.com>
2021-08-23 18:04:29 +0300

    last

--- xutil/loader_get_v3.py
+++ xutil/loader_get_v3.py
@@ -80,9 +80,9 @@ if __name__ == "__main__":
 
 
     url1 = 'https://sickjunk.com/'
-    url1a = 'https://rapelust.com/video/forced-anal-pain-factory/'
+    url1a = 'https://www.its.porn/video/enormous-knockers-fucked/'
     url2 = 'https://19-17.b.cdn13.com/021/042/728/720p.h264.mp4?cdn_creation_time=1628600400&cdn_ttl=14400&cdn_bw=378k&cdn_bw_fs=402k&cdn_cv_data=217.66.158.192-dvp&cdn_hash=730442c465665fb787f262193f04c484'
-    url3 = 'https://a.adtng.com/get/10009021?time=1575323689465'
+    url3 = 'https://www.youngpornvideos.com/embed/NpdV5yKdZuz'
 
 
     fname1 = 'out/1.html'
